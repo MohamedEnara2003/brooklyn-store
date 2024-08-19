3aa7bcd8f2e4c7e295aa1a6fb4f0a89cb9e17086
mohamedenara22 <midoahmeda673@gmail.com>
2024-08-19 20:00:12 +0300

    add changes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-    domains : [
-    "townteam.com",
-    "static.zara.net",
-    "https://flowbite.s3.amazonaws.com",
-    "img.freepik.com",
-    "www.freepik.com"]
+    remotePatterns: [
+    {
+    protocol: "https",
+    hostname: "**",
+    },
+    ],
     }
 };
 
