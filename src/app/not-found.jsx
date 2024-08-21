54b20f14351057b920228ff4f35a9a404cf4f6f5
mohamedenara22 <midoahmeda673@gmail.com>
2024-08-21 23:46:02 +0300

    changes

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -5,6 +5,7 @@ import Footer from './components/footer';
 import { t } from 'i18next';
 import  '../i18n';
 import { useTranslation } from 'react-i18next';
+import Link from 'next/link';
 function Notfound() {
     const [showSearch , setShowSearch] = useState()
     const [t , il8n] = useTranslation()
@@ -14,7 +15,7 @@ function Notfound() {
     <div className=" relative w-full h-[70vh] bg-white flex flex-col justify-center items-center">
     <h2 className=' uppercase font-[900] text-4xl'>404</h2>
     <h1 className='sale uppercase font-[900] '>{t('Page not found')}</h1>
-    <button className='btn_AddCart font-bold rounded-sm mt-20 '>{t('continue shopping')}</button>
+    <Link href="/" className='btn_AddCart font-bold rounded-sm mt-20 '>{t('continue shopping')}</Link>
     </div>
     <Footer/>
     </>
